d524033f7fa2cd0de1edac90f5646c2fec7dd3e5
Jakob Kirsch <jakob.kirsch@web.de>
2021-02-12 16:21:07 +0100

    Add suggestions from flork

--- kernel/paging.c
+++ kernel/paging.c
@@ -7,7 +7,6 @@ static uint64_t p3[512] __attribute__((aligned(4096)));
 static uint64_t p2s[2][512] __attribute__((aligned(4096)));
 
 void paging_setup() {
-	arch_disable_paging();
 	arch_invalidate_cr3();
 	for (int i = 0; i < 512; i++) {
 		p4[i] = 0;
@@ -22,10 +21,9 @@ void paging_setup() {
 
 	for (int j = 0; j < 2; j++) {
 		for (int i = 0; i < 512; i++) {
-			p2s[j][i] = 0x200000 * i | 0b10000011;
+			p2s[j][i] = 0x200000 * (512 * j + i) | 0b10000011;
 		}
 	}
 
 	arch_set_cr3(&p4);
-	arch_enable_paging();
 }
